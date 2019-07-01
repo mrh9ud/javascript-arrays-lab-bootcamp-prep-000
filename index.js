5b052a53123ca5ae61c457935fbc95ec73be9fd9
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-01 03:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 // Add your functions and code here
-function destructivelyAppendKitten(array, name) {
-  
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
 }
